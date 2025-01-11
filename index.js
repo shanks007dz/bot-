2f66a58a1dc92f34a8f2ac0c087b06e679d401ef
shanks007dz <mohamedlivefr95@gmail.com>
2025-01-11 03:43:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -37,8 +37,8 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["",  ""];
-const statusTypes = [ '', ''];
+const statusMessages = [""];
+const statusTypes = [ ''];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
 
